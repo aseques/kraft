dc9dbbae5fa69be2a5699db8e35aa8307b4ccdbe
Klaas Freitag <freitag@suse.de>
2018-07-13 17:47:34 +0200

    Addition to version 22 migrate script.

--- database/mysql/migration/22_dbmigrate.sql
+++ database/mysql/migration/22_dbmigrate.sql
@@ -1,4 +1,5 @@
 # Add a predecessor column to the documents table
 
 ALTER TABLE document ADD COLUMN predecessor VARCHAR(32) AFTER projectLabel;
+ALTER TABLE archdoc  ADD COLUMN predecessor VARCHAR(32) AFTER projectLabel;
 
